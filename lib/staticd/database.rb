e5712b063cff4bdb073b6e3d405f7ef15c46971a
Etienne Garnier <garnier.etienne@gmail.com>
2014-10-18 19:02:29 +0200

    add per environment database configuration

--- lib/staticd/database.rb
+++ lib/staticd/database.rb
@@ -1,13 +1,25 @@
 require "data_mapper"
 
 module Staticd
-  DataMapper.setup(:default, 'sqlite::memory:')
 
   # Load models
   require "staticd/model/site"
   require "staticd/model/release"
   require "staticd/model/domain_name"
 
+  case ENV["RACK_ENV"]
+  when "test"
+    DataMapper.setup(:default, "sqlite::memory:")
+  else
+    puts "Running database in development mode"
+    DataMapper::Logger.new($stdout, :debug)
+    DataMapper.setup(:default, "sqlite:///tmp/staticd-development.db")
+  end
+
   DataMapper.finalize
-  DataMapper.auto_migrate!
+  if ENV["RACK_ENV"] == "test"
+    DataMapper.auto_migrate!
+  else
+    DataMapper.auto_upgrade!
+  end
 end

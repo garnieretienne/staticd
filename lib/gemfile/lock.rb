628acc414e0550e52e807aa4d3cc57bbb14dd337
Etienne Garnier <garnier.etienne@gmail.com>
2014-11-06 00:55:39 +0100

    fix gemspec files to work when no gemfile is present

--- lib/gemfile/lock.rb
+++ lib/gemfile/lock.rb
@@ -4,11 +4,12 @@ module Gemfile
   class Lock
 
     def initialize(path)
-      @content = File.read path
+      @content = File.exist?(path) ? File.read(path) : nil
     end
 
     # Find the requirements of the gem used by bundler
     def find_requirements(gem_name)
+      return [] unless @content
       pattern = /    #{gem_name} \((.*)\)/
       begin
         @content.scan(pattern).first.first.split(", ")

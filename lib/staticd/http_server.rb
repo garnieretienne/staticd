d2cdd411badc8642c202855c6f653b4aff18d2f1
Etienne Garnier <garnier.etienne@gmail.com>
2014-11-11 19:41:59 +0100

    add supported mime-type and set the content-type header for response in http server

--- lib/staticd/http_server.rb
+++ lib/staticd/http_server.rb
@@ -1,6 +1,17 @@
 module Staticd
   class HTTPServer
 
+    EXT_MIME_TYPE = {
+      ".html" => "text/html",
+      ".css"  => "text/css",
+      ".js"   => "application/javascript",
+      ".jpg"  => "image/jpeg",
+      ".png"  => "image/png",
+      ".gif"  => "image/gif"
+    }
+
+    DEFAULT_MIME_TYPE = 'application/octet-stream'
+
     def initialize(http_root)
       @http_root = http_root
     end
@@ -16,6 +27,7 @@ module Staticd
     def send(file)
       if File.readable? file
         res = Rack::Response.new
+        res["Content-Type"] = mime file
         File.foreach(file){|chunk| res.write chunk}
         res.finish
       else
@@ -26,6 +38,11 @@ module Staticd
     def send_404
       res = Rack::Response.new ["Not Found"], 404, {}
       res.finish
+    end
+
+    def mime(file)
+      ext = File.extname(file).downcase
+      EXT_MIME_TYPE.has_key?(ext) ? EXT_MIME_TYPE[ext] : DEFAULT_MIME_TYPE
     end
   end
 end

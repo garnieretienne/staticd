5222ad1841f388df079bf2381b634ff9e008d99b
Etienne Garnier <garnier.etienne@gmail.com>
2014-11-20 19:26:50 +0100

    document and fix style in the Gemfile::Lock class

--- lib/gemfile/lock.rb
+++ lib/gemfile/lock.rb
@@ -1,21 +1,27 @@
 module Gemfile
 
-  # Class to analyze the content of a Gemfile.lock file
+  # This class analyze the content of a Gemfile.lock file.
+  #
+  # It can be used into gemspec files to retrieve current version of gem used by
+  # bundler.
+  #
+  # Example:
+  #   gemfile = Gemfile::Lock.new("#{File.dirname(__FILE__)}/Gemfile.lock")
+  #   s.executables << 'exe_name'
+  #     s.add_runtime_dependency gem_name, *gemfile.find_requirements(gem_name)
+  #   end
   class Lock
 
     def initialize(path)
-      @content = File.exist?(path) ? File.read(path) : nil
+      @content = File.read(path) if File.exist?(path)
     end
 
-    # Find the requirements of the gem used by bundler
     def find_requirements(gem_name)
       return [] unless @content
-      pattern = /    #{gem_name} \((.*)\)/
-      begin
-        @content.scan(pattern).first.first.split(", ")
-      rescue
-        []
-      end
+
+      @content.scan(/    #{gem_name} \((.*)\)/).first.first.split(", ")
+    rescue
+      []
     end
   end
 end

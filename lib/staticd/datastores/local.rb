9061e3701f42e3a316de4085e67261060b0466ce
Etienne Garnier <garnier.etienne@gmail.com>
2014-11-21 15:30:27 +0100

    document and fix style in the Staticd::Datastores::Local class

--- lib/staticd/datastores/local.rb
+++ lib/staticd/datastores/local.rb
@@ -1,39 +1,47 @@
-require 'digest/sha1'
+require "digest/sha1"
 
 module Staticd
   module Datastores
+
+    # Datastore storing file on local directory.
+    #
+    # It use the file SHA1 digest as a filename so two identical files are not
+    # stored twice.
+    #
+    # Example:
+    #   datastore = Staticd::Datastores::Local.new(path: "/tmp/datastore")
+    #   datastore.put(file_path) unless datastore.exist?(file_path)
+    #   # => "/tmp/datastore/sha1_digest"
     class Local
 
       def initialize(params)
         @path = params[:path]
-        verify_store_path
+        check_store_directory
       end
 
       def put(file_path)
-        if File.exist? file_path
-          FileUtils.copy_file file_path, stored_file_path(file_path)
-          stored_file_path(file_path)
-        end
+        datastore_file = stored_file_path(file_path)
+        FileUtils.copy_file(file_path, datastore_file) unless exist?(file_path)
+        datastore_file
       end
 
       def exist?(file_path)
-        if File.exist? stored_file_path(file_path)
-          stored_file_path(file_path)
-        end
+        datastore_file = stored_file_path(file_path)
+        File.exist?(datastore_file) ? datastore_file : false
       end
 
       private
 
-      def verify_store_path
+      def check_store_directory
         Dir.mkdir(@path) unless File.directory?(@path)
       end
 
       def sha1(file_path)
-        Digest::SHA1.hexdigest File.read(file_path)
+        Digest::SHA1.hexdigest(File.read(file_path))
       end
 
       def stored_file_path(file_path)
-        stored_file_path = "#{@path}/#{sha1(file_path)}"
+        "#{@path}/#{sha1(file_path)}"
       end
     end
   end

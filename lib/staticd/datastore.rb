bc11ad5cabc50fe8ed4388ae69d670d823ddb2ff
Etienne Garnier <garnier.etienne@gmail.com>
2014-11-25 23:34:44 +0100

    document and fix style in the Staticd::Datastore class

--- lib/staticd/datastore.rb
+++ lib/staticd/datastore.rb
@@ -1,8 +1,22 @@
 require "uri"
-require "staticd/datastores/local"
-require "staticd/datastores/s3"
+
+# Load datastores
+Dir["#{File.dirname(__FILE__)}/datastores/*.rb"].each do |datastore_library|
+  require datastore_library
+end
 
 module Staticd
+
+  # Load the corresponding datastore driver from an URL.
+  #
+  # This class use an URL to choose wich datastore library to use.
+  # It create a datastore instance with the correct driver and proxies its
+  # calls to it.
+  # It use the URL scheme to guess wich datastore library to use.
+  #
+  # Example:
+  #   Staticd::Datastore.new("s3://[...]") => Staticd::Datastores::S3
+  #   Staticd::Datastore.new("local:/[...]") => Staticd::Datastores::Local
   class Datastore
 
     def initialize(url)
@@ -10,11 +24,11 @@ module Staticd
     end
 
     def put(file_path)
-      datastore.put file_path
+      datastore.put(file_path)
     end
 
     def exist?(file_path)
-      datastore.exist? file_path
+      datastore.exist?(file_path)
     end
 
     private
@@ -24,12 +38,12 @@ module Staticd
     end
 
     def datastore
-      @datastore ||= datastoreClass.new({
+      @datastore ||= datastoreClass.new(
         host: @uri.host,
         path: @uri.path,
         username: @uri.user,
         password: @uri.password
-      })
+      )
     end
   end
 end

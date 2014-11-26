591a2fb79f6c4fcfc187bd6b1d984afe6ee80e5f
Etienne Garnier <garnier.etienne@gmail.com>
2014-11-26 17:17:02 +0100

    document and fix style in the StaticdUtils::GLIObject class

--- lib/staticd_utils/gli_object.rb
+++ lib/staticd_utils/gli_object.rb
@@ -1,6 +1,14 @@
 require 'gli'
 
-# Move the GLI::App module into its own class
+# Move the GLI::App module into its own class.
+#
+# It's usefull to build a GLI::App like object.
+#
+# Example:
+#   gli = GLIObject.new
+#   gli.program_desc("My Ultimate CLI")
+#   gli.version("1.0")
+#   gli.run(*args)
 class GLIObject
   include GLI::App
 end

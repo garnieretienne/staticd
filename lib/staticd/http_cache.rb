29c40e8931287f724325b08eecf0e753785bab88
Etienne Garnier <garnier.etienne@gmail.com>
2014-11-26 16:27:37 +0100

    document and fix style in the Staticd::HTTPCache class

--- lib/staticd/http_cache.rb
+++ lib/staticd/http_cache.rb
@@ -1,8 +1,13 @@
 require "staticd/database"
 require "staticd/cache_engine"
 
-# Rack middleware to manage remote resource caching
 module Staticd
+
+  # Rack middleware to manage remote HTTP resources caching.
+  #
+  # For each request, it will find the last release of the site associed with
+  # the requested domain name and cache the corresponding resource if not
+  # already cached.
   class HTTPCache
     include Staticd::Models
 
@@ -18,34 +23,32 @@ module Staticd
     def _call(env)
       @env = env
 
-      # Change the Request Path to '/index.html' if root is asked
-      @env["PATH_INFO"] = '/index.html' if @env["PATH_INFO"] == '/'
+      # Change the Request Path to '/index.html' if root path is asked.
+      @env["PATH_INFO"] = "/index.html" if @env["PATH_INFO"] == '/'
 
-      # Get the release from the request Host header
+      # Get the release from the request Host header.
       release = Release.last(
         Release.site.domain_names.name => Rack::Request.new(@env).host
       )
       return next_middleware unless release
 
-      # Change the script name to include the site name and release version
-      @env["SCRIPT_NAME"] = "/#{release.site_name}/#{release.tag}"
+      # Change the script name to include the site name and release version.
+      @env["SCRIPT_NAME"] = "/#{release.site_name}/#{release}"
 
       req = Rack::Request.new(@env)
       cache_engine = CacheEngine.new(@http_root)
 
-      # Do nothing else if the resource is already cached
-      if cache_engine.cached?(req.path)
-        return next_middleware
-      end
+      # Do nothing else if the resource is already cached.
+      return next_middleware if cache_engine.cached?(req.path)
 
-      # Get the resource to cache
-      resource = Resource.first({
+      # Get the resource to cache.
+      resource = Resource.first(
         Resource.routes.release_id => release.id,
         Resource.routes.path => req.path_info
-      })
+      )
       return next_middleware unless resource
 
-      # Cache the resource
+      # Cache the resource.
       cache_engine.cache(req.path, resource.url)
       next_middleware
     end
@@ -53,7 +56,7 @@ module Staticd
     private
 
     def next_middleware
-      status, headers, response = @app.call(@env)
+      @app.call(@env)
     end
   end
 end

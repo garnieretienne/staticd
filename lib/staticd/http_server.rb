1969e47106fb1ef06f2027f25857cb21a6e1468a
Etienne Garnier <garnier.etienne@gmail.com>
2014-11-26 16:41:04 +0100

    document and fix style in the Staticd::HTTPServer class

--- lib/staticd/http_server.rb
+++ lib/staticd/http_server.rb
@@ -1,11 +1,15 @@
 require "sendfile"
 
 module Staticd
+
+  # Simple HTTP server Rack app.
+  #
+  # If the resource is readable from the root folder at the path given by the
+  # request, the resource is sent to the client. Otherwise a 404 Not Found HTTP
+  # error is sent.
   class HTTPServer
 
-    # Mime types served by the webserver
-    # Take from NGiNX mime.types file.
-    DEFAULT_MIME_TYPE = 'application/octet-stream'
+    # Mime types served by the webserver (from NGiNX mime.types file).
     EXT_MIME_TYPE = {
       ".html"    => "text/html",
       ".htm"     => "text/html",
@@ -73,6 +77,9 @@ module Staticd
       ".mng"     => "video/x-mng"
     }
 
+    # Mime type used when no type has been identified.
+    DEFAULT_MIME_TYPE = "application/octet-stream"
+
     def initialize(http_root)
       @http_root = http_root
     end
@@ -83,30 +90,30 @@ module Staticd
 
     def _call(env)
       @env = env
-      req = Rack::Request.new @env
+      req = Rack::Request.new(@env)
       file_path = @http_root + req.path
-
-      if File.readable? file_path
-        sendfile file_path
-      else
-        send_404
-      end
+      File.readable?(file_path) ? sendfile(file_path) : send_404
     end
 
     private
 
+    # Send a file loading it in memory.
+    #
+    # Method used in the first implementation.
+    # Keep it for compatibility purpose when the Rack hijack API is not
+    # supported.
     def send(file_path)
       response = Rack::Response.new
-      response["Content-Type"] = mime file_path
-      File.foreach(file_path){|chunk| response.write chunk}
+      response["Content-Type"] = mime(file_path)
+      File.foreach(file_path) { |chunk| response.write(chunk) }
       response.finish
     end
 
     # Use sendfile system call to send file without loading it into memory.
     #
     # It use the sendfile gem and the rack hijacking api.
-    # see: https://github.com/codeslinger/sendfile
-    # see: http://blog.phusion.nl/2013/01/23/the-new-rack-socket-hijacking-api/
+    # See: https://github.com/codeslinger/sendfile
+    # See: http://blog.phusion.nl/2013/01/23/the-new-rack-socket-hijacking-api/
     def sendfile(file_path)
       return send(file_path) unless @env['rack.hijack']
 
@@ -114,16 +121,16 @@ module Staticd
       io = @env['rack.hijack_io']
 
       # Rescue: Do not raise a Errno::EPIPE: Broken pipe - sendfile if
-      # transfert is canceled by the client
+      # transfert is canceled by the client.
       begin
         io.write("HTTP/1.1 200 OK\r\n")
         io.write("Connection: close\r\n")
         io.write("Content-Type: #{mime(file_path)}\r\n")
         io.write("Content-Length: #{size(file_path)}\r\n")
         io.write("\r\n")
-        File.open(file_path){|file| io.sendfile file}
+        File.open(file_path) { |file| io.sendfile(file) }
         io.flush
-      rescue
+      rescue Errno::EPIPE => e
         true
       ensure
         io.close
@@ -131,13 +138,13 @@ module Staticd
     end
 
     def send_404
-      res = Rack::Response.new ["Not Found"], 404, {}
+      res = Rack::Response.new(["Not Found"], 404, {})
       res.finish
     end
 
     def mime(file_path)
       ext = File.extname(file_path).downcase
-      EXT_MIME_TYPE.has_key?(ext) ? EXT_MIME_TYPE[ext] : DEFAULT_MIME_TYPE
+      EXT_MIME_TYPE.key?(ext) ? EXT_MIME_TYPE[ext] : DEFAULT_MIME_TYPE
     end
 
     def size(file_path)

def5d76519d5e3af01b20d9e4aa7bedd1386edd5
Etienne Garnier <garnier.etienne@gmail.com>
2014-12-03 13:33:08 +0100

    document and fix style in the Staticdctl::RESTClient class

--- lib/staticdctl/rest_client.rb
+++ lib/staticdctl/rest_client.rb
@@ -4,6 +4,17 @@ require "json"
 
 module Staticdctl
 
+  # Simple REST client library with HMAC authentication.
+  #
+  # Support for remote call on JSON REST API.
+  #
+  # Example:
+  #   client = Staticdctl::RESTCLient.new(
+  #     url: "http://api.domain.tld/v1",
+  #     access_id: 1000,
+  #     secret_key: "youshallnotpass"
+  #   )
+  #   client.call(:get, "/resources") { |response| puts response }
   class RESTClient
 
     def initialize(url, hmac={})
@@ -12,55 +23,63 @@ module Staticdctl
       @secret_key = hmac[:secret_key] || ""
     end
 
+    # Call a remote REST API action.
+    #
+    # Example:
+    #   client.call(:post, "/posts", {text: "hello_world"}) do |response|
+    #     puts response
+    #   end
     def call(method, path, req_data=nil, &block)
-      headers = {}
-      headers["Accept"] = "application/json"
+      headers = {
+        "Accept" => "application/json"
+      }
       headers["Content-Type"] = "application/json" if req_data
       payload = req_data ? req_data.to_json : nil
       request = RestClient::Request.new(
-        url: "#{@url}#{path}",
+        url: @url + path,
         method: method,
         headers: headers,
         payload: payload
       )
-
-      send_request request, block
+      send_request(request, block)
     end
 
+    # Send files using the HTTP multipart/form-data content-type.
+    #
+    # Example:
+    #   client.send_files("/attachments", {first: file1, second: file2})
     def send_files(path, files, &block)
       headers = {
        "Accept" => "application/json",
        "Content-Type" => "multipart/form-data"
       }
       request = RestClient::Request.new(
-        url: "#{@url}#{path}",
+        url: @url + path,
         method: :post,
         headers: headers,
         payload: files,
         timeout: -1
       )
-
-      send_request request, block
+      send_request(request, block)
     end
 
     private
 
-    def send_request(request, procedure)
+    def send_request(request, &block)
       signed_request = ApiAuth.sign!(request, @access_id, @secret_key)
-
       signed_request.execute do |response, request, result|
         res_data = JSON.parse(response.to_s) unless response.to_s.empty?
         case response.code
         when 200
-          procedure.call res_data
+          block.call(res_data)
         when 204
-          procedure.call
+          block.call
         when 403
-          raise res_data['error']
+          raise res_data["error"]
         when 401
-          raise res_data['error']
+          raise res_data["error"]
         else
-          raise "Server returned an '#{response.code}' status code"
+          raise "Server returned an '#{response.code}' status code."
         end
       end
     end
